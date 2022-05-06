2f7115a6c4db57da684b3263f49963554c5d862e
a.a.pligina <87080361+AnastasiaPlg@users.noreply.github.com>
2022-05-06 15:58:49 +0300

    Add files via upload

---
+++ Task9/src/Task9.java
@@ -0,0 +1,14 @@
+public class Task9 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 7;
+        int z = 0;
+        int k = 9;
+        z = (int)Math.pow(x, 2) + (int)Math.pow(y, 2);
+        System.out.println(z);
+        z = (int)Math.pow(x, 3);
+        System.out.println(z);
+        k = x / z;
+        System.out.println(k);
+    }
+}
