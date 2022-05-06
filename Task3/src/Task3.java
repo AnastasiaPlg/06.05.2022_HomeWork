e5720bd71647e063670f16607c8cf9c084b51247
a.a.pligina <87080361+AnastasiaPlg@users.noreply.github.com>
2022-05-06 14:52:48 +0300

    Add files via upload

---
+++ Task3/src/Task3.java
@@ -0,0 +1,7 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 10;
+        System.out.println(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2)));
+    }
+}
