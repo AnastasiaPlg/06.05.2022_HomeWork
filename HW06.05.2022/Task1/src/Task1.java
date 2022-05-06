18137c6259737ffa8c831278c95d134f1b992ae5
Anastasia <a.a.pligina@gmail.com>
2022-05-06 12:36:47 +0300

    AddTask2a

---
+++ HW06.05.2022/Task1/src/Task1.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите цену товара А:");
+        double priceA = scanner.nextDouble();
+        System.out.println("Введите цену товара Б:");
+        double priceB = scanner.nextDouble();
+
+        System.out.println("Задание 1а: " + taskA(priceA, priceB));
+        System.out.println("Задание 1б: " + taskB(priceA, priceB));
+
+    }
+
+    public static double taskA (double xx, double yy) {
+        int coinsNumberProductA = (int) (xx * 100);
+        int coinsNumberProductB = (int) (yy * 100);
+        double saleA = 0.15;
+        int coinsFinalPriceProductA = (int)((1 - saleA) * coinsNumberProductA);
+        int coinsFinalCost = 10 * coinsFinalPriceProductA + 2 * coinsNumberProductB;
+        double finalPrice = (double)coinsFinalCost / 100;
+        return finalPrice;
+    }
+
+    public static double taskB (double xx, double yy) {
+        int coinsNumberProductA = (int) (xx * 100);
+        int coinsNumberProductB = (int) (yy * 100);
+        double saleA = 0.5;
+        int coinsFinalPriceProductA = (int)((1 - saleA) * coinsNumberProductA);
+        int coinsFinalCost = 7 * coinsFinalPriceProductA + 1 * coinsNumberProductB;
+        double finalPrice = (double)coinsFinalCost / 100;
+        return finalPrice;
+    }
+}
+
+
